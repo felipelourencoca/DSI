7ef3f05da627ec0a224a6eb6ce00fc90f8bef0ec
Felipe Canarozzo Lourenço <floureno@FATECRL.COM>
2022-09-14 08:03:30 -0300

    Aulas faltantes

---
+++ 4-classe-objeto-atributo-metodo/src/FormaBolo.java
@@ -0,0 +1,32 @@
+
+public class FormaBolo {
+	//Atributos,características do tipo é
+	private String sabor;
+	private float  peso;
+	
+	public FormaBolo(String sabor, float peso) {
+		this.sabor = sabor;
+		this.peso = peso;
+	}
+	
+	/*
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	} */
+	
+	public String getSabor() {
+		return this.sabor;
+	}
+	
+	/*
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}*/
+	public float getPeso() {
+		return this.peso;
+	}
+	
+	
+	
+
+}
