bc4c8db6dd0a2d33abe38bc4562b18456c5bb753
Felipe Canarozzo Lourenço <floureno@FATECRL.COM>
2022-11-09 09:31:13 -0300

    Inicio do projeto Telefonia

---
+++ Telefonia/src/Chamada.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Chamada {
+	private Date data;
+	private Integer duracao;
+	
+	public Chamada(Date data, Integer duracao) {
+		super();
+		this.data = data;
+		this.duracao = duracao;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public Integer getDuracao() {
+		return duracao;
+	}
+
+	@Override
+	public String toString() {
+		return "Chamada [data=" + data + ", duracao=" + duracao + "]";
+	}
+	
+	
+	public static void main(String[] args) {
+		Calendar ca = Calendar.getInstance();
+		ca.set(1999,01,01);		
+		Date d = ca.getTime();
+		Chamada c = new Chamada(d, 50);
+		Chamada c2 = new Chamada(d, 50);
+		
+		
+		
+		List<Chamada> chamadas = new ArrayList<>();
+		chamadas.add(c);
+		chamadas.add(c2);
+		Set cm = new HashSet();
+		
+		cm.add(c);
+		cm.add(c2);
+		
+		System.out.println(cm);
+		System.out.println(chamadas);
+		//System.out.println(c);
+		
+	
+		
+	}
+	
+}
